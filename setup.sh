f9d36afc025aba1724881acd154281aefb58f946
renatoagds <renato_0603@hotmail.com>
2018-02-01 16:06:30 -0200

    use correct google casks

--- setup.sh
+++ setup.sh
@@ -52,7 +52,8 @@ declare -a cask=(
     "skype" 
     "the-unarchiver"
     "adobe-acrobat-reader"
-    "google-drive-file-stream"
+    "google-photos-backup-and-sync"
+    "google-backup-and-sync"
     "android-studio"
   )
 

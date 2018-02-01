27a1b45695a733603ad9d3a801afd7ce13ff2004
Renato Augusto <loggi@renatoagds.local>
2018-02-01 00:50:54 -0200

    add setup scripts and remove some aliases

---
+++ loggi-setup.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# renatoagds (Setup loggi stuffs )
+LOGGI=/opt/loggi
+USER=loggi
+
+# login into registry
+echo "npm login"
+npm login
+
+# docker login (docker needs to be open)
+echo "docker login"
+docker login
+
+# folder setup
+echo "creating loggi folder"
+sudo mkdir -p "${LOGGI}"
+sudo chown -R "${USER}" "${LOGGI}"
+sudo chmod -R 755 "${LOGGI}"
+
+declare -a front=(
+    "corp-web-ui"
+    "wms-web-ui"
+    "retail-web-ui"
+    "elke"
+    "outer-pages-ui"
+    "javascript"
+    "consumer-app"
+    "wms-terminal-app-1"
+    "wms-terminal-app"
+    "ui-builder"
+    "tracker-web-ui"
+    "auth-web-ui"
+    "shell-web-ui"
+    "email-web-ui"
+  )
+
+# clone and setup front repos
+echo "fronts repos"
+for i in "${front[@]}"; do
+  cd "${LOGGI}"
+  git clone "git@github.com:loggi/${i}.git"
+  cd "${i}"
+  npm i
+done
+
+declare -a others=(
+    "ops"
+    "loggi-web"
+  )
+
+echo "others repos"
+cd "${LOGGI}"
+for i in "${others[@]}"; do
+  git clone "git@github.com:loggi/${i}.git"
+done
+
+loggi up
+loggi setup-dev-db
+
---
+++ setup.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# renatoagds (SH to setup basic stuffs in new Mac OS)
+DOTFILES=~/Documents/Personal/dotfiles
+
+# go to main folder
+cd ~
+
+# install brew if isn't installed
+if ! type "$brew" &> /dev/null; then
+  echo "brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# setup oh-my-zsh
+echo "oh-my-zsh"
+curl -O https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+cat install.sh | awk 'NR!=113{print $0}' | sh
+rm -rf ~/.zshrc
+ln -s "${DOTFILES}/zsh/zshrc" ~/.zshrc
+rm -rf install.sh
+
+declare -a brew=(
+    "nvm"
+    "neovim"
+    "mitmproxy"
+    "python2"
+    "python3"
+    "git"
+    "tmux"
+    "zsh"
+    "zsh-completion"
+    "ack"
+    "shpotify"
+    "docker"
+    "cmake"
+  )
+
+declare -a cask=(
+    "google-chrome"
+    "iterm2"
+    "firefox"
+    "dash"
+    "docker"
+    "spectacle"
+    "evernote"
+    "telegram"
+    "whatsapp"
+    "slack"
+    "spotify"
+    "tuxera-ntfs"
+    "skype" 
+    "the-unarchiver"
+    "adobe-acrobat-reader"
+    "dropbox"
+    "android-studio"
+  )
+
+# install all brew dep
+echo "brew"
+for i in "${brew[@]}"; do
+  brew install "$i"
+done
+
+# install all cask dep
+echo "brew-cask"
+for i in "${cask[@]}"; do
+  brew cask install "$i"
+done
+
+# setup neovim
+echo "neovim"
+pip2 install neovim
+pip3 install neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim/
+for f in ${DOTFILES}/vim/*.vim; do
+  filename=$(basename "$f")
+  ln -s "$f" "${HOME}/.config/nvim/${filename}"
+done
+
+# setup tmux
+echo "tmux"
+ln -s "${DOTFILES}/tmux/tmux.conf" ~/.tmux.conf
+
+# export NVM
+export NVM_DIR="$HOME/.nvm"
+. "$(brew --prefix nvm)/nvm.sh"
+
+# install node/npm
+echo "node/npm"
+nvm install node
+nvm use node
+nvm alias default node
+
+# setup modules
+echo "npm"
+declare -a npm=(
+    "react-native-cli"
+    "code-push-cli"
+    "appcenter-cli"
+    "npm-check-updates"
+    "http-server"
+  )
+
+for i in "${npm[@]}"; do
+  npm i -g "$i"
+done

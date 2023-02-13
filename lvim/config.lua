4e8e5013da40c8d9d9a06203309ed96c0ff2413c
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-02-13 17:07:15 -0300

    Increase timeout

--- lvim/config.lua
+++ lvim/config.lua
@@ -154,7 +154,7 @@ code_actions.setup {
 
 lvim.builtin.which_key.mappings["l"]["f"] = {
   function()
-    require("lvim.lsp.utils").format { timeout_ms = 2000 }
+    require("lvim.lsp.utils").format { timeout_ms = 5000 }
   end,
   "Format",
 }

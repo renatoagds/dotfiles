0abddb182f9e84c0fcad9acab66ed4bf17b4f723
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2023-04-20 14:14:30 -0300

    Update HOP commands

--- vim/lua/config/hop.lua
+++ vim/lua/config/hop.lua
@@ -3,13 +3,19 @@ local wk = require 'which-key'
 wk.register({
   ['<leader><leader>'] = {
     name = '+Hop',
-    w = {
+    -- waiting for: https://github.com/phaazon/hop.nvim/issues/335
+    -- to return using AC and BC
+    --[[ w = {
       ':HopWordAC<CR>',
       'Jump to Word After Cursor'
     },
     b = {
       ':HopWordBC<CR>',
       'Jump to Word Before Cursor'
+    }, ]]
+    w = {
+      ':HopWord<CR>',
+      'Jump to any word',
     },
   },
 })

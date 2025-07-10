cf5b581e040d92da9685e6d09148b71e1af307db
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2025-07-10 10:08:53 -0300

    feat: add copilot configuration for lua

---
+++ vim/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "saghen/blink.cmp",
+  opts = function(_, opts)
+    opts.appearance = opts.appearance or {}
+    opts.appearance.kind_icons = vim.tbl_extend("force", opts.appearance.kind_icons or {}, LazyVim.config.icons.kinds, {
+      Copilot = LazyVim.config.icons.kinds.Copilot or "",
+    })
+  end,
+}

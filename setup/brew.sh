46281c1d29c627c8daecef360cec5db6d26330bd
Renato Santos <renato.santos@HE-renato.santos-KW07W1QFYY>
2025-07-20 10:18:50 -0300

    feat: split setup in files

---
+++ setup/brew.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# renatoagds (SH to setup basic stuffs in new Mac OS)
+DOTFILES=~/Documents/Code/dotfiles
+
+# go to main folder
+cd ~
+
+# install brew if isn't installed
+if ! type "$brew" &> /dev/null; then
+  echo "brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+declare -a brew=(
+    "nvm"
+    "neovim"
+    "git"
+    "tmux"
+    "zsh"
+    "zsh-completion"
+    "watchman"
+    "yarn"
+    "highlight"
+    "bat"
+    "prettyping"
+    "ripgrep"
+    "fd"
+  )
+
+declare -a cask=(
+    "brave-browser"
+    "hyper"
+    "docker"
+    "rectangle"
+    "telegram"
+    "whatsapp"
+    "slack"
+    "spotify"
+    "keka"
+    "adobe-acrobat-reader"
+    "numi"
+    "1password"
+    "kap"
+    "vlc"
+  )
+
+# install all brew dep
+echo "Installing application from Brew"
+for i in "${brew[@]}"; do
+  brew install "$i"
+done
+
+# install all cask dep
+# echo "Installing application from Brew Cask"
+# brew tap caskroom/versions
+# for i in "${cask[@]}"; do
+  # brew install --cask "$i"
+# done
---
+++ setup/neovim.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# renatoagds (SH to setup basic stuffs in new Mac OS)
+DOTFILES=~/Documents/Code/dotfiles
+
+# go to main folder
+cd ~
+
+# install neovim
+echo "Installing LazyVim"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+echo "Linking neovim files"
+ln -s "${DOTFILES}/vim/colorscheme.lua" ~/.config/nvim/lua/plugins/colorscheme.lua
+ln -s "${DOTFILES}/vim/copilot.lua" ~/.config/nvim/lua/plugins/copilot.lua
+ln -s "${DOTFILES}/vim/lsp.lua" ~/.config/nvim/lua/plugins/lsp.lua
+ln -s "${DOTFILES}/vim/lazyvim.json" ~/.config/nvim/lua/plugins/lazyvim.json

904b4a7354e51e41d32fb659f6aaa73e3bba11b9
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2025-07-22 08:51:44 -0300

    feat: add more vscode config settings

--- vim/options.lua
+++ vim/options.lua
@@ -7,8 +7,18 @@ if vim.g.vscode then
 	vscode.update_config("editor.fontSize", 13, "global")
 	vscode.update_config("editor.lineNumbers", "relative", "global")
 	vscode.update_config("editor.fontFamily", "Fira Code", "global")
+	vscode.update_config("editor.formatOnSave", true, "global")
+	vscode.update_config("editor.wordWrap", "on", "global")
+
 	vscode.update_config("window.zoomLevel", 1, "global")
 	vscode.update_config("window.commandCenter", true, "global")
-	vscode.update_config("workbench.colorTheme", "Dracula Theme", "global")
-end
 
+	vscode.update_config("workbench.colorTheme", "Dracula Theme", "global")
+	vscode.update_config("workbench.activityBar.location", "default", "global")
+	vscode.update_config("workbench.activityBar.orientation", "vertical", "global")
+	vscode.update_config("workbench.sideBar.location", "right", "global")
+
+	vscode.update_config("search.followSymlinks", false, "global")
+
+	vscode.update_config("extensions.autoUpdate", "onlyEnabledExtensions", "global")
+end

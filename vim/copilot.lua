9a5388da61250769dde24297311fd72fb9c82884
Renato Augusto <renato_0603@hotmail.com>
2025-10-12 10:30:59 -0300

    feat: add yaml in copilot

--- vim/copilot.lua
+++ vim/copilot.lua
@@ -1,9 +1,20 @@
 return {
-  "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.appearance = opts.appearance or {}
-    opts.appearance.kind_icons = vim.tbl_extend("force", opts.appearance.kind_icons or {}, LazyVim.config.icons.kinds, {
-      Copilot = LazyVim.config.icons.kinds.Copilot or "",
-    })
-  end,
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.appearance = opts.appearance or {}
+      opts.appearance.kind_icons =
+        vim.tbl_extend("force", opts.appearance.kind_icons or {}, LazyVim.config.icons.kinds, {
+          Copilot = LazyVim.config.icons.kinds.Copilot or "",
+        })
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        yaml = true,
+      },
+    },
+  },
 }

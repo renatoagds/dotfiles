16d008b1a84b5b30e4129485ebf4bfd2491b8123
Renato Santos <renato.santos@Moxie-Macbook.local>
2025-10-24 16:10:41 -0300

    chore: update scm_breeze install code

--- setup/brew.sh
+++ setup/brew.sh
@@ -8,42 +8,42 @@ cd ~
 
 # install brew if isn't installed
 # if ! type "$brew" &> /dev/null; then
-  # echo "brew"
-  # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo "brew"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # fi
 
 declare -a brew=(
-    "nvm"
-    "neovim"
-    "git"
-    "tmux"
-    "zsh"
-    "zsh-completion"
-    "watchman"
-    "yarn"
-    "highlight"
-    "bat"
-    "prettyping"
-    "ripgrep"
-    "fd"
-  )
+  "nvm"
+  "neovim"
+  "git"
+  "tmux"
+  "zsh"
+  "zsh-completion"
+  "watchman"
+  "yarn"
+  "highlight"
+  "bat"
+  "prettyping"
+  "ripgrep"
+  "fd"
+)
 
 declare -a cask=(
-    "brave-browser"
-    "hyper"
-    "docker"
-    "rectangle"
-    "telegram"
-    "whatsapp"
-    "slack"
-    "spotify"
-    "keka"
-    "adobe-acrobat-reader"
-    "numi"
-    "1password"
-    "kap"
-    "vlc"
-  )
+  "brave-browser"
+  "hyper"
+  "docker"
+  "rectangle"
+  "telegram"
+  "whatsapp"
+  "slack"
+  "spotify"
+  "keka"
+  "adobe-acrobat-reader"
+  "numi"
+  "1password"
+  "kap"
+  "vlc"
+)
 
 # install all brew dep
 echo "Installing application from Brew"
@@ -55,10 +55,10 @@ done
 # echo "Installing application from Brew Cask"
 # brew tap caskroom/versions
 # for i in "${cask[@]}"; do
-  # brew install --cask "$i"
+# brew install --cask "$i"
 # done
 
 # setup scm_breeze
-git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
 source ~/.zshrc

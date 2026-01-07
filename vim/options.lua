6d13de4b9459619c03bf4e42b1828c010480c8dd
Renato Augusto <renato_0603@hotmail.com>
2026-01-07 08:09:08 -0300

    feat: update neovim config

--- vim/options.lua
+++ vim/options.lua
@@ -1,26 +1,22 @@
-local dracula_pro = "dracula_pro"
-
 vim.g.mapleader = ","
-vim.g.ai_cmp = false
-vim.g.dracula_colorterm = 0
-vim.opt.packpath:append(os.getenv("HOME") .. "/.local/share/nvim/site")
-vim.cmd("colorscheme " .. dracula_pro)
 
-if vim.g.vscode then
+if not vim.g.vscode then
+  local dracula_pro = "dracula_pro"
+  vim.g.ai_cmp = false
+  vim.g.dracula_colorterm = 0
+  vim.opt.packpath:append(os.getenv("HOME") .. "/.local/share/nvim/site")
+  vim.cmd("colorscheme " .. dracula_pro)
+else
   local vscode = require("vscode")
-
   vscode.update_config("editor.fontSize", 13, "global")
   vscode.update_config("editor.lineNumbers", "relative", "global")
   vscode.update_config("editor.fontFamily", "Fira Code", "global")
   vscode.update_config("editor.formatOnSave", true, "global")
   vscode.update_config("editor.wordWrap", "on", "global")
-
   vscode.update_config("window.zoomLevel", 1, "global")
   vscode.update_config("window.commandCenter", true, "global")
-
   vscode.update_config("workbench.colorTheme", "Dracula Pro", "global")
   vscode.update_config("workbench.activityBar.orientation", "vertical", "global")
   vscode.update_config("workbench.sideBar.location", "left", "global")
-
   vscode.update_config("search.followSymlinks", false, "global")
 end

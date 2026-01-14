b0a2363eb66864867b9b472a0807970914617ae8
Renato Augusto <renato_0603@hotmail.com>
2026-01-13 22:13:36 -0300

    feat: update vscode mappings

--- vim/keymaps.lua
+++ vim/keymaps.lua
@@ -1,18 +1,18 @@
-if vim.g.vscode then
-	-- VSCode
-	local vscode = require("vscode")
-
-	vim.keymap.set("n", "<leader>cr", function()
-		vscode.action("editor.action.rename")
-	end)
-
-	vim.keymap.set("n", "<leader>w", function()
-		vscode.action("workbench.action.files.save")
-	end)
-
-	vim.keymap.set("n", "<leader>bd", function()
-		vscode.action("workbench.action.closeActiveEditor")
-	end)
+if not vim.g.vscode then
+  -- regular NeoVim
 else
-	-- regular NeoVim
+  -- VSCode
+  local vscode = require("vscode")
+
+  vim.keymap.set("n", "<leader>cr", function()
+    vscode.action("editor.action.rename")
+  end)
+
+  vim.keymap.set("n", "<leader>w", function()
+    vscode.action("workbench.action.files.save")
+  end)
+
+  vim.keymap.set("n", "<leader>bd", function()
+    vscode.action("workbench.action.closeActiveEditor")
+  end)
 end

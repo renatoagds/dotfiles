67b556db04ea619b4c3b37fcda307a6d0b02addc
Renato Augusto <renato_0603@hotmail.com>
2026-01-30 08:52:28 -0300

    feat: update dotfiles

--- vim/options.lua
+++ vim/options.lua
@@ -1,8 +1,10 @@
 vim.g.mapleader = ","
 
 if not vim.g.vscode then
+  -- general
+  vim.g.ai_cmp = true
+  -- dracula
   local dracula_pro = "dracula_pro"
-  vim.g.ai_cmp = false
   vim.g.dracula_colorterm = 0
   vim.opt.packpath:append(os.getenv("HOME") .. "/.local/share/nvim/site")
   vim.cmd("colorscheme " .. dracula_pro)

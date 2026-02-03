332c058d98714bbed8d6bc5125152a473df40c37
Renato Augusto <renato_0603@hotmail.com>
2026-02-03 09:24:14 -0300

    feat: add keymap to copy line and range for AI

--- vim/keymaps.lua
+++ vim/keymaps.lua
@@ -1,5 +1,29 @@
 if not vim.g.vscode then
-  -- regular NeoVim
+  -- Normal Mode: Copy "@path:line"
+  vim.keymap.set("n", "<leader>cy", function()
+    local path = vim.fn.expand("%:.")
+    local line = vim.fn.line(".")
+    local result = string.format("@%s:%s", path, line)
+    vim.fn.setreg("+", result)
+    vim.notify("Copied: " .. result)
+  end, { desc = "Copy @Path:Line" })
+
+  -- Visual Mode: Copy "@path:start-end" (same format as normal)
+  vim.keymap.set("v", "<leader>cy", function()
+    local path = vim.fn.expand("%:.")
+    local sline = vim.fn.line("v")
+    local eline = vim.fn.line(".")
+
+    -- Ensure sline is the smaller number
+    if sline > eline then
+      sline, eline = eline, sline
+    end
+
+    local result = string.format("@%s:%d-%d", path, sline, eline)
+
+    vim.fn.setreg("+", result)
+    vim.notify("Copied: " .. result)
+  end, { desc = "Copy @Path:Range" })
 else
   -- VSCode
   local vscode = require("vscode")

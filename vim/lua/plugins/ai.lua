0fdaaf96123028f56d3f9c1b5aac5f0a894550f2
Renato Augusto <renato_0603@hotmail.com>
2026-02-26 00:01:22 -0300

    feat: start usage of cursortab.nvim

--- vim/lua/plugins/ai.lua
+++ vim/lua/plugins/ai.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+  {
+    "leonardcser/cursortab.nvim",
+    -- version = "*",  -- Use latest tagged version for more stability
+    lazy = false, -- The server is already lazy loaded
+    build = "cd server && go build",
+    config = function()
+      require("cursortab").setup({
+        provider = {
+          type = "copilot",
+        },
+      })
+    end,
+  },
+}

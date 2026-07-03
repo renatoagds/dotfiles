68e2cf0c6c376af011e0614a8e7b2d0954ed24b2
Renato Augusto <renato.gamadossantos@joinmoxie.com>
2026-07-03 11:59:20 -0300

    feat: remove cursortab

--- vim/lua/plugins/ai.lua
+++ vim/lua/plugins/ai.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "leonardcser/cursortab.nvim",
-    -- version = "*",  -- Use latest tagged version for more stability
-    lazy = false, -- The server is already lazy loaded
-    build = "cd server && go build",
-    opts = {
-      provider = {
-        type = "copilot",
-      },
-      keymaps = {
-        accept = "<leader>aw",
-        partial_accept = "<leader>aq",
-      },
-    },
-  },
-  {
     "folke/sidekick.nvim",
     opts = {
       nes = {
